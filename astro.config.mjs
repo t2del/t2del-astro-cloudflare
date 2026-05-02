f469a94c3dfca922389e6f3536cacfd9b85e0b6f
Dennis Macapagal <macapagal.dennis@gmail.com>
2026-05-02 21:13:42 +0800

    fix minor layout changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://t2del.com",
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap() ],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
