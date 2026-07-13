8728741ddfe573a7aa13337220d25c6645543339
Dennis Macapagal <macapagal.dennis@gmail.com>
2026-07-13 20:40:24 +0800

    updated sitemap-blog

--- src/pages/sitemap-blog.xml.js
+++ src/pages/sitemap-blog.xml.js
@@ -10,7 +10,7 @@ export async function GET() {
   
   // 3. Construct XML
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
       ${urls.map(url => `<url><loc>${url}</loc></url>`).join('')}
     </urlset>`;
 
